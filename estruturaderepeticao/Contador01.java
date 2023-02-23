58d4bbcbe4e2f46fe01a69c7fa3e859d17ca42b8
Erlan Charles Furtado <erlanfurtado7@gmail.com>
2023-02-22 21:18:41 -0300

    estrutura de repetição 1

---
+++ estruturaderepeticao/Contador01.java
@@ -0,0 +1,13 @@
+package estruturaderepeticao;
+
+public class Contador01 {
+
+	public static void main(String[] args) {
+		
+		int cc = 1;
+		while (cc<=4) {
+			System.out.println("cabalhota " + cc);
+			cc++;
+		}
+	}
+}
